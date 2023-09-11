3cf2768c5ba12ed24da067cc21a119e53e362d98
Haris <hellomharis@gmail.com>
2023-09-11 09:38:55 +0530

    finished mvp1

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import CommandPalette, { JsonStructure, filterItems, getItemIndex, } from "react
 import { useState, useEffect, Dispatch, SetStateAction } from "react";
 // import { renderJsonStructure, useHandleOpenCommandPalette } from "./lib/utils";
 import { renderJsonStructure, useHandleOpenCommandPalette } from "react-cmdk";
+import Image from "next/image";
 
 enum CommandPalettePage {
   Root = "ROOT",
@@ -214,8 +215,44 @@ const Example = () => {
 
 
       </CommandPalette>
+      {/* Other text contents */}
+      <div className="">
+        <h1 className="mt-6 md:mt-10 text-center text-xl md:text-3xl">أسلم عليكم</h1>
 
-      <h1>jslkdfj</h1>
+        <div className="flex top-3 md:top-8 items-center gap-4 right-5 md:right-10 fixed ">
+          <p className="hidden md:block">Muhammad Afzal</p>
+          <div className="w-[50px] h-[50px] bg-red-300 rounded-full overflow-hidden ">
+            {/* <img src="/profile.jpeg" alt="sdfsd" /> */}
+            <Image
+              src="/profile.jpeg"
+              width={50}
+              height={50}
+              alt="Picture of the user"
+            />
+          </div>
+        </div>
+
+        <h1 className="text-center mt-12 px-6 leading-10 text-[2.5rem] font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-purple-500 to-cyan-600">Superpower <span className="text-white">to your finger</span></h1>
+        <p className="text-gray-400 text-lg text-center mt-4">Find at the speed of thought</p>
+
+        {/* keyboard icons */}
+        <div className="hidden md:flex mt-4 justify-center w-full">
+          <img src="/ctrlk.svg" alt="" />
+        </div>
+        <p className="hidden md:block text-gray-400 text-sm mt-2 text-center">One magic shortcut</p>
+
+
+        <div className="flex items-center  gap-4 absolute bottom-16 ml-[50%] -translate-x-[50%]">
+          <p className="text-gray-400 text-sm">Tools Used</p>
+          <img className="" src="/next.png" alt="" width="22px" height="22px" />
+          <img className="" src="/ts.png" alt="" width="22px" height="22px" />
+          <img className="" src="/tw.png" alt="" width="22px" height="22px" />
+        </div>
+
+        <p className="text-gray-400 absolute text-sm  w-full md:text-normal bottom-4 flex justify-center">Made with 💖 by <a href="http://www.barmej.tech" target="_blank">Haris bin Husain</a></p>
+
+      </div>
+
     </>
   );
 };
